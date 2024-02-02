59f67287eaae2ded056833eb3f682b5c90eb4955
KK <crosskyhu@gmail.com>
2024-02-02 14:46:19 +0800

    6.0. Servlet 瘦身优化（if - else）

--- src/com/kk/furns/web/MemberServlet.java
+++ src/com/kk/furns/web/MemberServlet.java
@@ -1,5 +1,6 @@
 package src.com.kk.furns.web;
 
+import com.sun.net.httpserver.HttpServer;
 import src.com.kk.furns.entity.Member;
 import src.com.kk.furns.service.MemberService;
 import src.com.kk.furns.service.impl.MemberServiceImpl;
@@ -14,7 +15,7 @@ import java.io.IOException;
  * @author KK
  * @version 1.0
  */
-public class LoginServlet extends HttpServlet {
+public class MemberServlet extends HttpServlet {
     private MemberService memberService = new MemberServiceImpl();
 
     @Override
@@ -24,6 +25,20 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if ("login".equals(action)) {
+            login(req, resp);
+        } else if ("register".equals(action)) {
+            register(req, resp);
+        } else {
+            System.out.println("Action 参数不正确");
+        }
+    }
+
+    /**
+     * 用户登录服务
+     */
+    public void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         Member member = new Member(null, username, password, null);
@@ -33,6 +48,25 @@ public class LoginServlet extends HttpServlet {
             req.setAttribute("msg", "用户名或密码错误");
             req.setAttribute("username", username);
             req.getRequestDispatcher("/views/member/login.jsp").forward(req, resp);
+        }
+    }
+
+    /**
+     * 用户注册服务
+     */
+    public void register(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        Member member = new Member(null, username, password, email);
+        if (memberService.userRegister(member)) {
+            // 注册成功
+            req.getRequestDispatcher("/views/member/register_ok.html")
+                    .forward(req, resp);
+        } else {
+            // 注册失败
+            req.getRequestDispatcher("/views/member/register_fail.html")
+                    .forward(req, resp);
         }
     }
 }
